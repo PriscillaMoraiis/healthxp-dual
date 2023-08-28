88532d7bdf3d3e01a7d6b7bd08f8ea31d650b55e
Priscilla Morais <prisciillamoraiis@gmail.com>
2023-08-28 10:55:18 -0300

    my first commit

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,58 @@
+import users from "../fixtures/users.json";
+
+import login from "../support/pages/LoginPage";
+
+describe("login", () => {
+    it("deve logar com o perfil admin", () => {
+        const user = users.admin;
+
+        login.doLogin(user);
+
+        cy.contains("aside .logged-user", "Olá, " + user.name).should("be.visible");
+    });
+
+    it("não deve logar com senha incorreta", () => {
+        const user = users.inv_pass;
+
+        login.doLogin(user);
+        login.popUpHave(
+            "Suas credenciais são inválidas, por favor tente novamente!"
+        );
+    });
+
+    it("não deve logar com email não cadastrado", () => {
+        const user = users.email_not_found;
+
+        login.doLogin(user);
+        login.popUpHave(
+            "Suas credenciais são inválidas, por favor tente novamente!"
+        );
+    });
+
+    it("não deve logar com emails incorretos", () => {
+        const emails = users.inv_emails;
+
+        login.go();
+
+        emails.forEach((u) => {
+            login.fill(u);
+            login.submit();
+            login.popUpHave("Insira um email válido.");
+            login.popUpBack();
+        });
+    });
+
+    it("não deve logar com email vazio", () => {
+        const user = users.empty_email;
+
+        login.doLogin(user);
+        login.popUpHave("Os campos email e senha são obrigatórios.");
+    });
+
+    it("não deve logar com senha vazia", () => {
+        const user = users.empty_password;
+
+        login.doLogin(user);
+        login.popUpHave("Os campos email e senha são obrigatórios.");
+    });
+});
